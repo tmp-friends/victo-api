28837143bda40095fc72c9788c840f3055d49c7a
komekami <komekami0131@gmail.com>
2023-03-12 05:47:35 +0000

    feat: usersテーブル定義の追加

---
+++ scripts/migrations/000002_create_users_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `hash_uid` VARCHAR(191) NOT NULL UNIQUE,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `profile_image_url` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
